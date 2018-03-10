6e3963fceefb45648b10e44b40b2e49d179e9d6f
Edward Nimocks <enimocks@gmail.com>
2018-03-10 16:33:25 -0500

    test

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Variables
+var workBank = [
+  'targaryen',
+  'stark',
+  'lannister',
+  'arryn',
+  'tully',
+  'greyjoy',
+  'baratheon',
+  'tyrell',
+  'martell'
+];
+
+var wins = 0;
+var losses = 0;
